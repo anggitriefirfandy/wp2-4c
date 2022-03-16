09ecba3a2dcbc05bb390e861e518ef8f8fd4b4c1
anggitriefirfandy <A.R.Irfandy2000@gmail.com>
2022-03-16 16:54:22 +0700

    first

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$conn_str = "host=hansken.db.elephantsql.com" .
+"port=5432" .
+"user=mlvgkndg" .
+"dbname=mlvgkndg". 
+"password=ubsNFYJ9XUfpC76O0N9kkYVkGO7Wt7FG";
+$conn = pg_connect($conn_str);
+
+if($conn){
+    echo "<h3>Koneksi berhasil</h3>";
+}else{
+    echo"<h3>Koneksi GAGAL TERSAMBUNG</h3>";
+}
+?>
